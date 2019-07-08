45ba2fd685096649539dc92cdae3eb0a467c8a7e
hidemaro nakano <vita.hima-hima-0427@ezweb.ne.jp>
2019-07-08 21:36:03 +0900

    add: basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+  protect_from_forgery with: :exception
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["CHAT_SPACE_USER"] && password == ENV["CHAT_SPACE_PASSWORD"]
+    end
+  end
 
   protected
 
